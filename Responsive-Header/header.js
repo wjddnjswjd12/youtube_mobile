b2b757ec79f6f2657f05a864507c97667c057c67
Jungwon Lee <leejungwon96@gmail.com>
2020-12-29 00:24:44 +0900

    toggle button

--- Responsive-Header/header.js
+++ Responsive-Header/header.js
@@ -1,1 +1,8 @@
 const toggleBtn = document.querySelector(".navbar_toggleBtn");
+const menu = document.querySelector(".nav");
+const sns = document.querySelector(".sns");
+
+toggleBtn.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  sns.classList.toggle("active");
+});
